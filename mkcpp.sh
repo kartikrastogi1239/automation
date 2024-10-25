422b7005bd4812125e875f44a35f1b5c06323fb7
kartikrastogi1239 <kartikrastogi1239@gmail.com>
2024-10-25 23:30:57 +0530

    updated version

--- mkcpp.sh
+++ mkcpp.sh
@@ -3,13 +3,13 @@
 function check_dependencies() {
     if ! command -v g++ &> /dev/null; then
         echo "g++ not found, installing..."
-        apt-get update
-	sudo apt-get install g++
+        sudo apt-get update
+        sudo apt-get install -y g++
     fi
 
     if ! command -v scrot &> /dev/null; then
         echo "scrot not found, installing..."
-        sudo apt-get install scrot
+        sudo apt-get install -y scrot
     fi
 
     if ! python3 -c "import docx" &> /dev/null; then
@@ -20,35 +20,56 @@ function check_dependencies() {
 
 check_dependencies
 
+# Prompt for user inputs
 echo "Your roll no: "
 read roll_no
-echo "your name: "
+echo "Your name: "
 read name
-count=$(find Desktop/ass_test/codes -type f -name "*.cpp" | wc -l)
 
-mkdir -p Desktop/ass_test/outputs
-FOLDER="Desktop/ass_test/outputs"
+# Count the number of C++ files in the codes directory
+count=$(find "${HOME}/Desktop/ass_test/codes" -type f -name "*.cpp" | wc -l)
+
+
+# Create output directory if it doesn't exist
+mkdir -p "${HOME}/Desktop/ass_test/outputs"
+FOLDER="${HOME}/Desktop/ass_test/outputs"
+clear
+# Loop to compile and execute each C++ file
 for (( i=1; i<=$count; i++ ))
 do
-
-     g++ Desktop/ass_test/codes/q$i.cpp -o ~/Desktop/ass_test/codes/a.out
-     if [ $? -eq 0 ]; then
-        ~/Desktop/ass_test/codes/a.out
-         FILENAME="OUTPUT_$i.png"
-         sleep 1
-         scrot "$FOLDER/$FILENAME"
-         echo "Screenshot saved as $FOLDER/$FILENAME"
-     else
-         echo "Compilation failed for q$i.cpp"
-     fi
-     clear
+    # Compile the C++ file
+    g++ "${HOME}/Desktop/ass_test/codes/q$i.cpp" -o "${HOME}/Desktop/ass_test/codes/a.out"
+    if [ $? -eq 0 ]; then
+        # Run the compiled output
+        "${HOME}/Desktop/ass_test/codes/a.out"
+        FILENAME="OUTPUT_$i.png"
+        sleep 1
+        # Take a screenshot
+        scrot "$FOLDER/$FILENAME"
+        echo "Screenshot saved as $FOLDER/$FILENAME"
+    else
+        echo "Compilation failed for q$i.cpp"
+    fi
+    clear
 done
-mv /home/kartik/Desktop/ass_test /home/kartik/Desktop/24mcf1r19_kartik
-mkdir /home/kartik/Desktop/ass_test
-cp -rv /home/kartik/Desktop/24mcf1r19_kartik/codes /home/kartik/Desktop/ass_test
-python3.13 generate_doc.py
-cd Desktop
-mv /home/kartik/Desktop/24mcf1r19_kartik /home/kartik/Desktop/24mcf1r${roll_no}_${name}
-zip -r 24mcf1r${roll_no}_${name}.zip 24mcf1r${roll_no}_${name}
-mv Code_and_Output_Document.docx 24mcf1r${roll_no}_${name}.docx
 
+
+# Move and rename the directory
+mv "${HOME}/Desktop/ass_test" "${HOME}/Desktop/24mcf1r19_kartik"
+mkdir -p "${HOME}/Desktop/ass_test"
+cp -rv "${HOME}/Desktop/24mcf1r19_kartik/codes" "${HOME}/Desktop/ass_test"
+
+# Run the Python script to generate the docx file
+python3.13 "${HOME}/automation/generate_doc.py"
+
+# Change to the Desktop directory
+cd "${HOME}/Desktop"
+
+# Rename the directory with the roll number and name
+mv "${HOME}/Desktop/24mcf1r19_kartik" "${HOME}/Desktop/24mcf1r${roll_no}_${name}"
+
+# Create a zip file of the renamed directory
+zip -r "24mcf1r${roll_no}_${name}.zip" "24mcf1r${roll_no}_${name}"
+echo "document has succesfully created THANK YOU for using this"
+# Rename the generated docx file
+mv "Code_and_Output_Document.docx" "24mcf1r${roll_no}_${name}.docx"
